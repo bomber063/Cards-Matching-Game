041c6bcae87f9e375850cf8b727d0af04d0747a2
bomber <tangyihong063@163.com>
2019-03-22 14:40:26 +0800

    抖动效果完成删除多余代码弹出胜利界面可以重置

--- js/app.js
+++ js/app.js
@@ -12,10 +12,6 @@ for (var i = 0; i < cards.length; i++) {
   $(".deck").append(cards[i]);
 }
 
-
-
-
-
 // 设置一个用于存放打开卡片的数组
 var openedCard = [];
 // 设置一个用于存放打开并匹配卡片的数组
@@ -24,14 +20,7 @@ var matchedCard = [];
 function myFunction() {
   setTimeout(myFunctions2, 200);
   //关闭
-  function myFunctions2() {//怎么才能选择opencard数组中的$(".card .open .show").removeClass("open show")
-    // 这里$(.card空格.open空格.show) 和 $(.card逗号.open逗号.show)
-    // $(openedCard).attr('.card,.open,.show").removeClass("open,show");
-    // $(".card,.open,.show").removeClass("open,show");//如果点击的两次所取得类不一样，就返回继续关闭。也就是移除open和show的class。
-       // $(openedCard).find('i').removeClass("open show");
-
-
-    // console.log($(".card,.open,.show").removeClass("open show"));
+  function myFunctions2() {
     openedCard.forEach(function(card){
       card.attr("class","card open show shake");
       setTimeout(function(){
@@ -64,10 +53,10 @@ var matching = function () {
 
     };
 
-                    if(matchedCard.length == 4) { //停止计时函数
+        if(matchedCard.length == 2) { //停止计时函数
                       
 
- $("div").on("click","i",function()
+ $("#dd").on("click","i",function()
    // oStart.onclick= function () 
 {
     clearInterval(timer);
@@ -90,9 +79,18 @@ HideStar(a);
                         win();
                         document.getElementById("Endtimes").innerHTML = oTxt.value;
                         document.getElementById("Endsteps").innerHTML = a;
-                      }
-  console.log(a);
+                      }                  
+
+console.log(this);
+
 };
+
+
+
+
+
+
+
 
 
 // $('.card').on('click', function (e) {　如果click后面没有selector就是绑定事件，点击事件一开始就会调用函数
@@ -112,33 +110,17 @@ HideStar(a);
       $(this).toggleClass("open show");
       var op = $(this);//.find("i");//.attr("class");// 提取所点击对象的子元素i的class类
       openedCard.push(op);//把提取的图片的类放到数组openedCard里面去
-
-
-
-// time1 function{
-//        if(openedCard.length>0){
-//         console.log(openedCard);
-//            function timerout(){  //开始
-//      time=setInterval(timer,50);
-//      console.log(setInterval(timer,50));
-               
-//    }
-//        }
-
-// }
       matching();
     }
   };
 
 });
 
-   $('.restart').on('click','i',function () {　//如果click后面有selector就是委托事件，点击事件一开始不会调用函数，需要通过冒泡到card才会调用函数。
-       // $('.card').on('click','li',function (e) {　//改成这样的委托事件就打不开卡牌了。
-  // console.log(e.target) -> HTML dom
-  // console.log($(this))  -> Jquery
-  // console.log( $('.card') ) -> Jquery
-var restart=$('.deck').find('li').attr('class');
 
+   $('.restart').on('click','i',function () {　
+// var restart=$('.deck').find('li').attr('class');
+
+       $(".card.open.show.match.rubberBand").removeClass('open show match rubberBand');
        $(".card.open.show.match").removeClass('open show match');
        $(".card.open.show").removeClass('open show');
        $(".card.open").removeClass('open');     
@@ -155,50 +137,73 @@ var restart=$('.deck').find('li').attr('class');
      $(".deck").append(cards[i]);
    }
 
- $("div").on("click","i",function()
-   // oStart.onclick= function () 
-{
+
     clearInterval(timer);
     oTxt.value="00:00:00";
     n=0;
-    // setInterval() 方法可按照指定的周期（以毫秒计）来调用函数或计算表达式。
-//     timer=setInterval(function () 
-//     {
-//         n++;
-//         var m=parseInt(n/3600)
-//         var s=parseInt(n/60%60);
-//         var M=parseInt(n%60);
-//         oTxt.value=toDub(m)+":"+toDub(s)+":"+toDub(M);
-//     },1000/60);
-});
+
 
 a=0;
         numberOfsteps(a);
         a=1;
+matchedCard=[];//重置后需要把匹配的卡片重新赋值为空，要不然一直都是2都是会显示胜利界面。
 });
 
-// var restart=function(){
-//   var cards = [];
-// for (var i = 0; i < $('.card').length; i++) {
-//   cards.push($('.card')[i]);
-// }
-
-// cards = shuffle(cards);
-// $(".card").remove();
-// for (var i = 0; i < cards.length; i++) {
-//   $(".deck").append(cards[i]);
-//   return cards;
-//   }
-// }
-
-// var oStart=$('ul li');
 
 
-var oTxt=document.getElementsByTagName("input")[0];
-// var oStart=document.getElementsByTagName("input")[1];
-// var oStart=$("input")[1];
-var oStop=document.getElementsByTagName("input")[2];
-var oReset=document.getElementsByTagName("input")[3];
+$('.restartWin').on('click','i',function () {　
+// var restartWin=$('.deck').find('li').attr('class');
+
+       $(".card.open.show.match.rubberBand").removeClass('open show match rubberBand');
+       $(".card.open.show.match").removeClass('open show match');
+       $(".card.open.show").removeClass('open show');
+       $(".card.open").removeClass('open');     
+
+           
+   var cards = [];
+   for (var i = 0; i < $('.card').length; i++) {
+     cards.push($('.card')[i]);
+   }
+
+   cards = shuffle(cards);
+   $(".card").remove();
+   for (var i = 0; i < cards.length; i++) {
+     $(".deck").append(cards[i]);
+   }
+
+
+
+
+
+
+
+    clearInterval(timer);
+    oTxt.value="00:00:00";
+    n=0;
+
+
+a=0;
+        numberOfsteps(a);
+        a=1;
+winadd();
+matchedCard=[];
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var oTxt=document.getElementsByTagName("input")[0];//TagName用JQ的写法，取到的是一个数组吗？必须要给出[]?
+var oTxt=$("input")[0];//TagName用JQ的写法，后面必须要加[0]取到的是一个数组吗？取到的是一个数组吗？必须要给出[]
+
 
 var n= 0;
 var timer=null;
@@ -219,86 +224,20 @@ var timer=null;
     },1000/60);
 });
 //暂停并且清空计时器
-oStop.onclick= function () {
-    clearInterval(timer);
-}
-//重置
-oReset.onclick= function () {
-    oTxt.value="00:00:00";
-    n=0;
-}
+
+
 //补零
 function toDub(n){
     return n<10?"0"+n:""+n;
 }
 
-
-
-
-// oStart.onclick=function(){
-
-//    var h=m=s=ms= 0;  //定义时，分，秒，毫秒并初始化为0；
-//    var time=0;
- 
-//    function timer(){   //定义计时函数
-//      ms=ms+50;         //毫秒
-//      if(ms>=1000){
-//        ms=0;
-//        s=s+1;         //秒
-//      }
-//      if(s>=60){
-//        s=0;
-//        m=m+1;        //分钟
-//      }
-//      if(m>=60){
-//        m=0;
-//        h=h+1;        //小时
-//      }
-//      str =toDub(h)+"时"+toDub(m)+"分"+toDub(s)+"秒"+toDubms(ms)+"毫秒";
-//      mytime = document.getElementById('mytime');
-//      mytime.innerHTML = str;
-//    document.getElementById('mytime').innerHTML=h+"时"+m+"分"+s+"秒"+ms+"毫秒";
-//    }
- 
-//    // function reset(){  //重置
-//    //   clearInterval(time);
-//    //   h=m=s=ms=0;
-//    //   document.getElementById('mytime').innerHTML="00时00分00秒0000毫秒";
-//    // }
- 
-//    function start(){  //开始
-//      time=setInterval(timer,50);
-//    }
- 
-//    // function stop(){  //暂停
-//    //   clearInterval(time);
-//    // }
- 
-//    function toDub(n){  //补0操作
-//      if(n<10){
-//        return "0"+n;
-//      }
-//      else {
-//        return ""+n;
-//      }
-//    }
- 
-//    function toDubms(n){  //给毫秒补0操作
-//      if(n<10){
-//        return "00"+n;
-//      }
-//      else {
-//        return "0"+n;
-//      }
- 
-//    }
-
-// };
-
   var a=1;
 function numberOfsteps(a) {
 
-    document.getElementById('steps').innerHTML = a;
+    // document.getElementById('steps').innerHTML = a;//JS的写法
+       $('.moves').html(a);//JQ的写法
+              $('#steps').html(a);//JQ的写法
+
 }
 
 
@@ -344,12 +283,14 @@ function HideStar(a) {
 }
 
 
-
+function winadd() {
+    $(".containerWin").addClass("winDisply");
+};
 
 
 function win() {
     $(".containerWin").removeClass("winDisply");
-}
+};
 
 
 /*
